579d392e2b284dd88ab73e81749b77af4e88d43e
luijar <luijar@luisatencio.net>
2016-11-08 22:42:30 -0500

    Ch3

---
+++ examples/3/2/3_2.js
@@ -0,0 +1,36 @@
+/**
+ *  RxJS in Action
+ *  Listing 3.2
+ *  @author Paul Daniels
+ *  @author Luis Atencio
+ */
+const progressBar$ = Rx.Observable.create(observer => {
+   const OFFSET = 3000;
+   const SPEED =  50;
+
+   let val = 0;
+   let timeoutId = 0;
+   function progress() {
+     if(++val <= 100) {
+       observer.next(val);
+       timeoutId = setTimeout(progress, SPEED);
+     }
+     else {
+       observer.complete();
+     }
+   };
+   timeoutId  = setTimeout(progress, OFFSET);
+
+   return () => { //#A
+      clearTimeout(timeoutId);
+    };
+});
+
+//--------------------------------------------------//
+//                Usage                             //
+//--------------------------------------------------//
+const subs = progressBar$.subscribe(console.log, null, () => console.log('Complete!'));
+
+setTimeout(() => {
+  subs.unsubscribe();
+}, 6000);
