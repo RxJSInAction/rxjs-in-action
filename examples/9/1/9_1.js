b7ec6037fe0451a5006a3a59f335fce3b8150a89
luijar <luijar@luisatencio.net>
2016-11-16 17:51:08 -0500

    CH9

--- examples/9/1/9_1.js
+++ examples/9/1/9_1.js
@@ -8,6 +8,10 @@ mocha.setup({ ui: 'bdd', checkLeaks: true});
 
 const expect = chai.expect;
 
+function notEmpty(value) {
+  return typeof value == 'string' && !value.trim() || typeof value == 'undefined' || value === null;
+}
+
 describe('Validation', function () {
 	it('Should validate that a string is not empty', function() { //#B
 	   expect(notEmpty('some input')).to.be(true); //#B
