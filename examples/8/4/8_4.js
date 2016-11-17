5db993480c5cc99199083247fd7eda4e48455e7f
luijar <luijar@luisatencio.net>
2016-11-16 19:14:44 -0500

    CH9

--- examples/8/4/8_4.js
+++ examples/8/4/8_4.js
@@ -6,6 +6,40 @@
  */
 
  //http://jsbin.com/yusoya/50/edit?html,js,output
+const csv = str => str.split(/,\s*/);
+
+const Money = function (currency, val) {
+  return {
+    currency: function () {
+      return currency;
+    },
+    value: function () {
+      return val;
+    },
+    toString: function () {
+      return `${currency} ${val}`;
+    }
+  };
+};
+
+const ajax = url => new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function() {
+      if(req.status == 200) {
+        let data = req.responseText;
+        resolve(data);
+      }
+      else {
+        reject(new Error(req.statusText));
+      }
+    };
+    req.onerror = function () {
+      reject(new Error('IO Error'));
+    };
+    req.send();
+  });
+
 class StockTicker extends EventEmitter {
 
   constructor(symbol) {
@@ -41,10 +75,36 @@ const tick$ = Rx.Observable.fromEvent(ticker, 'tick',
         (symbol, price) => ({'symbol': symbol, 'price': price}))
    .catch(Rx.Observable.throw(new Error('Stock ticker exception')));
 
-const sub1 = ticks$.subscribe(
-   quoteDetails => updatePanel1(quoteDetails.symbol, quoteDetails.price)
-);
+const symbolElem = document.querySelector('#company');
+const priceElem = document.querySelector('#price');
 
-const sub1 = ticks$.subscribe(
-   quoteDetails => updatePanel2(quoteDetails.symbol, quoteDetails.price)
-);
+const sub1 = tick$.subscribe(
+       quoteDetails => {
+           const USDMoney = Money.bind(null, 'USD');
+           let  priceUSD = new USDMoney(quoteDetails.price);
+           priceElem.innerHTML = priceUSD.toString();
+           symbolElem.innerHTML = quoteDetails.symbol;
+         },
+         error => {
+           ticker.stop();
+         }
+      );
+
+const symbolElem2 = document.querySelector('#company2');
+const priceElem2 = document.querySelector('#price2');
+
+const sub2 = tick$.subscribe(
+    quoteDetails => {
+        const USDMoney = Money.bind(null, 'USD');
+        let  priceUSD = new USDMoney(quoteDetails.price);
+        priceElem2.innerHTML = priceUSD.toString();
+        symbolElem2.innerHTML = quoteDetails.symbol;
+      },
+      error => {
+        ticker.stop();
+      }
+   );
+
+setTimeout(() => {
+   ticker.stop();
+}, 10000);
