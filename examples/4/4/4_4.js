08d6fa0cb2763cb1dba132c7fd91aa99ce7ddc32
paulpdaniels <paulpdaniels@gmail.com>
2017-10-10 23:16:39 +0800

    Fixing bug in the transaction processor for 10.10

--- examples/4/4/4_4.js
+++ examples/4/4/4_4.js
@@ -23,7 +23,10 @@ const newRandomNumber = () => Math.floor(Math.random() * 100);
 const USDMoney = Money.bind(null, 'USD');
 
 Rx.Observable.interval(2000)
-   .skip(1)
-   .take(5)
-   .map(num => new USDMoney(newRandomNumber()))
-   .forEach(console.log);
+  .skip(1)
+  .take(5)
+  .map(num => new USDMoney(newRandomNumber()))
+  // WARNING: NOT IN TEXT
+  // Added to address #11 (https://github.com/RxJSInAction/rxjs-in-action/issues/11)
+  .map(usd => usd.toString())
+  .forEach(console.log);
